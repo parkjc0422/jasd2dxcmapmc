8a8a0619f2ce6bc6122b67e5392db2197f288524
jucher park <park.jc@kt.com>
2018-11-20 18:28:25 +0900

    Init

---
+++ brion/ios/KingdomOfCurling/DerivedData/KingdomOfCurling/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-KingdomOfCurling.build/DerivedSources/Pods_KingdomOfCurling_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_KingdomOfCurlingVersionString[];
+ extern const double Pods_KingdomOfCurlingVersionNumber;
+
+ const unsigned char Pods_KingdomOfCurlingVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_KingdomOfCurling  PROJECT:Pods-1" "\n";
+ const double Pods_KingdomOfCurlingVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ brion/ios/KingdomOfCurling/KingdomOfCurling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KingdomOfCurling
+//
+//  Created by jucher park on 11/11/2018.
+//  Copyright © 2018 jucher park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
